19b4e862c1df9fa1968e91e5e31424ee422029b9
Killian Biondo <biondo.killian@gmail.com>
2025-04-05 08:10:11 +0200

    create congé

--- public/src/Controller/CongeController.php
+++ public/src/Controller/CongeController.php
@@ -4,17 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CongeRepository;
 use App\Entity\Conge;
+use App\Form\CongeType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CongeController extends AbstractController
 {
     private $congeRepository;
+    private $entityManager;
 
-    public function __construct(CongeRepository $congeRepository)
+    public function __construct(CongeRepository $congeRepository, EntityManagerInterface $entityManager)
     {
         $this->congeRepository = $congeRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/conge', name: 'conge')]
@@ -53,13 +58,26 @@ class CongeController extends AbstractController
         ]);
     }
 
-    #[Route('/conge/create}', name: 'conge_create')]
-    public function congeCreate(): Response
+    #[Route('/conge/create', name: 'conge_create')]
+    public function create(Request $request): Response
     {
+        $conge = new Conge();
+        $form = $this->createForm(CongeType::class, $conge);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Par défaut, le statut est "En attente"
+            $conge->setStatut('En attente');
+
+            $this->entityManager->persist($conge);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Le congé a été créé avec succès!');
+            return $this->redirectToRoute('conge');
+        }
+
+        return $this->render('conge/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
-
-
-
-
 }
---
+++ public/src/Form/CongeType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conge;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CongeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function (User $user) {
+                    return $user->getNom() . ' ' . $user->getPrenom();
+                },
+                'label' => 'Employé',
+                'placeholder' => 'Sélectionnez un employé',
+                'required' => true,
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Congé payé' => 'Congé payé',
+                    'Congé maladie' => 'Congé maladie',
+                    'Congé sans solde' => 'Congé sans solde',
+                    'RTT' => 'RTT',
+                    'Autre' => 'Autre',
+                ],
+                'label' => 'Type de congé',
+                'required' => true,
+            ])
+            ->add('date_debut', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de début',
+                'required' => true,
+            ])
+            ->add('date_fin', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de fin',
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Conge::class,
+        ]);
+    }
+}
